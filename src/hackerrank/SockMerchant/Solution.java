2586216400c48d77c9cac6898b7b9fdffb23f18d
Boo <boo.mc@kakaocorp.com>
2019-06-25 17:58:19 +0900

    first commit

---
+++ src/hackerrank/SockMerchant/Solution.java
@@ -0,0 +1,30 @@
+package hackerrank.SockMerchant;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static int sockMerchant(int n, int[] ar) {
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+
+        for (int each : ar) {
+            if (set.contains(each)) {
+                set.remove(each);
+                count++;
+                continue;
+            }
+            set.add(each);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int n = 9;
+        int[] ar = new int[]{10, 20, 20, 10, 10, 30, 50, 10, 20};
+
+        int result = sockMerchant(9, ar);
+        System.out.println(result);
+    }
+}
+
